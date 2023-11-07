739b0636393561e612f1dfd44f0c2e564618ac3a
DebRC <roychowdhurydebdoot@gmail.com>
2023-11-07 22:49:31 +0530

    Modified script - numCores, poolSize

--- v3/server/runServer.sh
+++ v3/server/runServer.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <PortNum>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <NumberOfCores> <PortNum> <ThreadPoolSize> <RequestQueueSize>"
     exit 1
 fi
 
-port=$1
+core=$1
+port=$2
+threadPoolSize=$3
+requestQueueSize=$4
 
 mkdir -p logs
 mkdir -p logs/files
 mkdir -p logs/outputs
 
-gcc -o server server.c
+gcc -o server server.c circular_queue.c
+
+if [[ $core -eq 1 ]]; then
+    taskset -c 0 ./server $port $threadPoolSize $requestQueueSize
+fi
+if [[ $core -eq 2 ]]; then
+    taskset -c 0,1 ./server $port $threadPoolSize $requestQueueSize
+fi
+if [[ $core -eq 3 ]]; then
+    taskset -c 0,1,2 ./server $port $threadPoolSize $requestQueueSize
+fi
+if [[ $core -eq 4 ]]; then
+    taskset -c 0,1,2,3 ./server $port $threadPoolSize $requestQueueSize
+fi
 
 killChildren() {
     pkill -P $$  # Send the termination signal to all child processes
