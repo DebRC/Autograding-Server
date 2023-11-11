a3273af044c13993dfc74baf39cd1494758e30b7
Pranab239 <pranab4752@gmail.com>
2023-11-11 21:32:24 +0530

    updated plot.py

--- v3/client/plot.py
+++ v3/client/plot.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 num_of_clients = []
 response_time = []
 throughput_time = []
+overall_request_rate = []
+overall_error_rate = []
+overall_timeout_rate = []
+request_rate_sent = []
 
 with open(sys.argv[1],'r') as csv_file:
     content = csv.reader(csv_file)
@@ -13,6 +17,10 @@ with open(sys.argv[1],'r') as csv_file:
             num_of_clients.append(int(line[0]))
             response_time.append(float(line[1]))
             throughput_time.append(float(line[2]))
+            overall_request_rate.append(float(line[3]))
+            overall_error_rate.append(float(line[4]))
+            overall_timeout_rate.append(float(line[5]))
+            request_rate_sent.append(float(line[6]))
 
 
 # Clients Vs Throughput plot
@@ -25,6 +33,8 @@ plt.legend()
 plt.savefig("./observation/throughput.pdf", bbox_inches='tight')
 plt.close()
 
+
+
 # Clients Vs Response Time plot
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, response_time, label = "Clients Vs Average Response Time")
@@ -34,4 +44,54 @@ plt.legend()
 # plt.show()
 plt.savefig("./observation/avg_response_time.pdf", bbox_inches='tight')
 plt.close()
+
+
+
+# client Vs Overall_Request_Rate
+plt.figure(figsize=(8,6))
+plt.plot(num_of_clients, overall_request_rate, label = "Client Vs Overall Request Rate")
+plt.xlabel("Number of Clients")
+plt.ylabel("OVerall Request Rate")
+plt.legend()
+# plt.show()
+plt.savefig("./observation/overall_request_rate.pdf",bbox_inches='tight')
+plt.close()
+
+
+
+# client Vs Overall_error_Rate
+plt.figure(figsize=(8,6))
+plt.plot(num_of_clients, overall_error_rate, label = "Client Vs Overall Error Rate")
+plt.xlabel("Number of Clients")
+plt.ylabel("OVerall Error Rate")
+plt.legend()
+# plt.show()
+plt.savefig("./observation/overall_error_rate.pdf",bbox_inches='tight')
+plt.close()
+
+
+
+# client Vs Overall_timeout_Rate
+plt.figure(figsize=(8,6))
+plt.plot(num_of_clients, overall_timeout_rate, label = "Client Vs Overall Time-Out Rate")
+plt.xlabel("Number of Clients")
+plt.ylabel("OVerall Time-Out Rate")
+plt.legend()
+# plt.show()
+plt.savefig("./observation/overall_timeout_rate.pdf",bbox_inches='tight')
+plt.close()
+
+
+
+# client Vs Overall_Rate_Sent
+plt.figure(figsize=(8,6))
+plt.plot(num_of_clients, request_rate_sent, label = "Client Vs Request Rate Sent")
+plt.xlabel("Number of Clients")
+plt.ylabel("Request Rate Sent")
+plt.legend()
+# plt.show()
+plt.savefig("./observation/request_rate_sent.pdf",bbox_inches='tight')
+plt.close()
+
+
 
