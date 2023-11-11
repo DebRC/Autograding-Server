7e0a7e313c5e8e3dca8b84e3bee2f8426b8466ba
Pranab239 <pranab4752@gmail.com>
2023-11-12 00:13:35 +0530

    added utilization script for thread cpu

--- v3/client/loadtest.sh
+++ v3/client/loadtest.sh
@@ -25,6 +25,8 @@ overall_request_rate=0.00
 overall_timeout_rate=0.00
 total_response=0
 
+bash utilizationScript.sh 8080 &
+
 # Executing the client
 for ((num=1; num<=$numOfClient; num++)); do
     output_file="output_$num.txt"
@@ -37,7 +39,12 @@ for ((i=1; i<=$numOfClient; i++)); do
     wait "${pids[$i]}"
 done
 
+PID=$(ps -eLf | grep utilizationScript.sh | head -1 | awk '{print $2}')
+echo "PID: $PID"
+kill -9 $PID 
 
+# vmstat 1 2 | tail -1 | awk '{print $13}' >> cpu_utilization_snapshots.txt
+# ps -eLf | grep "./server $port_no" | head -1 | awk '{print $6}' >> thread_count_snapshots.txt
 
 
 # CALCULATING THE AVERAGE RESPONSE TIME
@@ -221,32 +228,32 @@ echo "Overall Request Rate Sent: $request_rate_sent"
 
 
 
-# # AVERAGE NO OF ACTIVE THREADS
-# sum=0
-# count=0
-# while read -r line; do
-#   sum=$((sum + line))
-#   count=$((count + 1))
-# done < thread_count_snapshots.txt
-# average_thread=$(echo "scale=2; $sum / $count" | bc -l)
-# echo "Average active threads: $average_thread"
-# > thread_count_snapshots.txt
+# AVERAGE NO OF ACTIVE THREADS
+sum=0
+count=0
+while read -r line; do
+  sum=$((sum + line))
+  count=$((count + 1))
+done < thread_count_snapshots.txt
+average_thread=$(echo "scale=2; $sum / $count" | bc -l)
+echo "Average active threads: $average_thread"
+> thread_count_snapshots.txt
 
 
 
 
 
-# # AVERAGE NO OF CPU UTILIZATION
-# sum=0
-# count=0
-# while read -r line; do
-#   sum=$((sum + line))
-#   count=$((count + 1))
-# done < cpu_utilization_snapshots.txt
-# average_cpu=$(echo "scale=2; $sum / $count" | bc -l)
-# echo "Average cpu utilization(%): $average_cpu"
-# > cpu_utilization_snapshots.txt
+# AVERAGE NO OF CPU UTILIZATION
+sum=0
+count=0
+while read -r line; do
+  sum=$((sum + line))
+  count=$((count + 1))
+done < cpu_utilization_snapshots.txt
+average_cpu=$(echo "scale=2; $sum / $count" | bc -l)
+echo "Average cpu utilization(%): $average_cpu"
+> cpu_utilization_snapshots.txt
 
 
 # Append the values to a csv file using >>
-echo $numOfClient,$avgResponseTime,$overall_throughput,$overall_request_rate,$overall_error_rate,$overall_timeout_rate,$request_rate_sent >> output.csv
+echo $numOfClient,$avgResponseTime,$overall_throughput,$overall_request_rate,$overall_error_rate,$overall_timeout_rate,$request_rate_sent,$average_thread,$average_cpu >> output.csv
