786a92936d11128f7daf8055321190d29b989374
DebRC <roychowdhurydebdoot@gmail.com>
2023-11-29 17:14:38 +0530

    Added units to graph

--- v3/client/plot.py
+++ v3/client/plot.py
@@ -2,7 +2,6 @@ import csv
 import sys
 import matplotlib.pyplot as plt
 
-# initialize the list for the plotting
 num_of_clients = []
 response_time = []
 throughput_time = []
@@ -13,7 +12,7 @@ request_rate_sent = []
 average_thread = []
 average_cpu = []
 
-# read data into lists from the csv file
+# open the csv file which contains all the necessary datas
 with open(sys.argv[1],'r') as csv_file:
     content = csv.reader(csv_file)
     for line in content:
@@ -33,7 +32,7 @@ with open(sys.argv[1],'r') as csv_file:
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, throughput_time, label = "Clients Vs Throughput")
 plt.xlabel("Number of Clients")
-plt.ylabel("Throughput")
+plt.ylabel("Throughput (Requests/Sec)")
 plt.legend()
 # plt.show()
 plt.savefig("./observation/throughput.pdf", bbox_inches='tight')
@@ -45,7 +44,7 @@ plt.close()
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, response_time, label = "Clients Vs Average Response Time")
 plt.xlabel("Number of Clients")
-plt.ylabel("Average Response Time")
+plt.ylabel("Average Response Time (Sec)")
 plt.legend()
 # plt.show()
 plt.savefig("./observation/avg_response_time.pdf", bbox_inches='tight')
@@ -57,7 +56,7 @@ plt.close()
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, overall_request_rate, label = "Client Vs Overall Request Rate")
 plt.xlabel("Number of Clients")
-plt.ylabel("OVerall Request Rate")
+plt.ylabel("OVerall Request Rate (Requests/Sec)")
 plt.legend()
 # plt.show()
 plt.savefig("./observation/overall_request_rate.pdf",bbox_inches='tight')
@@ -69,7 +68,7 @@ plt.close()
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, overall_error_rate, label = "Client Vs Overall Error Rate")
 plt.xlabel("Number of Clients")
-plt.ylabel("OVerall Error Rate")
+plt.ylabel("Overall Error Rate (Requests/Sec)")
 plt.legend()
 # plt.show()
 plt.savefig("./observation/overall_error_rate.pdf",bbox_inches='tight')
@@ -81,7 +80,7 @@ plt.close()
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, overall_timeout_rate, label = "Client Vs Overall Time-Out Rate")
 plt.xlabel("Number of Clients")
-plt.ylabel("OVerall Time-Out Rate")
+plt.ylabel("OVerall Time-Out Rate (Requests/Sec)")
 plt.legend()
 # plt.show()
 plt.savefig("./observation/overall_timeout_rate.pdf",bbox_inches='tight')
@@ -93,7 +92,7 @@ plt.close()
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, request_rate_sent, label = "Client Vs Request Rate Sent")
 plt.xlabel("Number of Clients")
-plt.ylabel("Request Rate Sent")
+plt.ylabel("Request Rate Sent (Requests/Sec)")
 plt.legend()
 # plt.show()
 plt.savefig("./observation/request_rate_sent.pdf",bbox_inches='tight')
@@ -117,7 +116,7 @@ plt.close()
 plt.figure(figsize=(8,6))
 plt.plot(num_of_clients, average_cpu, label = "Client Vs Average CPU")
 plt.xlabel("Number of Clients")
-plt.ylabel("Average CPU")
+plt.ylabel("Average CPU (Percentage)")
 plt.legend()
 # plt.show()
 plt.savefig("./observation/average_cpu.pdf",bbox_inches='tight')
