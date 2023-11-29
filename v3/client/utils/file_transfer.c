bf4d2cd6b6f4ecfc3d5488d6aa1de23d85371da3
Pranab239 <pranab4752@gmail.com>
2023-11-29 13:38:50 +0530

    fix utilization file transfer and server

--- v3/client/utils/file_transfer.c
+++ v3/client/utils/file_transfer.c
@@ -47,28 +47,56 @@ int recv_file(int sockfd, char *file_path)
     return 0;
 }
 
-
-
-int send_file(int sockfd, char *file_path)
-{
-    char buffer[BUFFER_SIZE];
-    bzero(buffer, BUFFER_SIZE);
-    FILE *file = fopen(file_path, "rb");
+int send_file(int sockfd, char* file_path) {
+    char buffer[BUFFER_SIZE]; //buffer to read  from  file
+    bzero(buffer, BUFFER_SIZE); //initialize buffer to all NULLs
+    FILE *file = fopen(file_path, "rb"); //open the file for reading, get file descriptor 
     if (!file)
     {
-        errorExit("ERROR :: FILE OPEN ERROR");
+        perror("Error opening file");
+        return -1;
     }
-    while (!feof(file))
+		
+		//for finding file size in bytes
+    fseek(file, 0L, SEEK_END); 
+    int file_size = ftell(file);
+    // printf("File size is: %d\n", file_size);
+    
+    //Reset file descriptor to beginning of file
+    fseek(file, 0L, SEEK_SET);
+		
+		//buffer to send file size to server
+    char file_size_bytes[MAX_FILE_SIZE_BYTES];
+    //copy the bytes of the file size integer into the char buffer
+    memcpy(file_size_bytes, &file_size, sizeof(file_size));
+    
+    //send file size to server, return -1 if error
+    if (send(sockfd, &file_size_bytes, sizeof(file_size_bytes), MSG_NOSIGNAL) == -1)
     {
-        size_t bytes_read = fread(buffer, 1, BUFFER_SIZE - 1, file);
-        if (send(sockfd, buffer, bytes_read + 1, MSG_NOSIGNAL) == -1)
+        perror("Error sending file size");
+        fclose(file);
+        return -1;
+    }
+
+	//now send the source code file 
+    while (!feof(file))  //while not reached end of file
+    {
+    
+    		//read buffer from file
+        size_t bytes_read = fread(buffer, 1, BUFFER_SIZE -1, file);
+        
+     		//send to server
+        if (send(sockfd, buffer, bytes_read+1, MSG_NOSIGNAL) == -1)
         {
+            perror("Error sending file data");
             fclose(file);
-            errorExit("ERROR :: FILE SEND ERROR");
+            return -1;
         }
+        
+        //clean out buffer before reading into it again
         bzero(buffer, BUFFER_SIZE);
     }
+    //close file
     fclose(file);
     return 0;
 }
-
--- v3/server/runServer.sh
+++ v3/server/runServer.sh
@@ -1,73 +1,43 @@
 #!/bin/bash
-# check if the inputs are correct or not
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <NumberOfCores> <PortNum> <ThreadPoolSize> <RequestQueueSize>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <NumberOfCores> <PortNum> <ThreadPoolSize>"
     exit 1
 fi
 
-# fetch the inputs
 core=$1
 port=$2
 threadPoolSize=$3
-requestQueueSize=$4
 
-# creating directories
+echo "Creating Required Directories ... "
 mkdir -p logs
 mkdir -p logs/files
 mkdir -p logs/outputs
 
-# run the server
-gcc -o server server.c utils/helper.c utils/circular_queue.c utils/make_filename.c utils/system_commands.c utils/file_transfer.c
-
-#handling the core
-if [[ $core -eq 1 ]]; then
-    taskset -c 0 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 2 ]]; then
-    taskset -c 0,1 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 3 ]]; then
-    taskset -c 0,1,2 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 4 ]]; then
-    taskset -c 0,1,2,3 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 5 ]]; then
-    taskset -c 0,1,2,3,4 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 6 ]]; then
-    taskset -c 0,1,2,3,4,5 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 7 ]]; then
-    taskset -c 0,1,2,3,4,5,6 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 8 ]]; then
-    taskset -c 0,1,2,3,4,5,6,7 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 9 ]]; then
-    taskset -c 0,1,2,3,4,5,6,7,8 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 10 ]]; then
-    taskset -c 0,1,2,3,4,5,6,7,8,9 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 11 ]]; then
-    taskset -c 0,1,2,3,4,5,6,7,8,9,10 ./server $port $threadPoolSize $requestQueueSize
-fi
-if [[ $core -eq 12 ]]; then
-    taskset -c 0,1,2,3,4,5,6,7,8,9,10,11 ./server $port $threadPoolSize $requestQueueSize
+echo "Creating Required Files ... "
+# Check if file exists
+if [ ! -e "request_status.csv" ]; then
+    touch "$filename"
 fi
 
-# kill all the child proceess
+echo "Running Make File ... "
+make
+
+echo "Done"
+
+echo "Running Server ... "
+
+if [[ $core -gt 0 && $core -le $(nproc) ]]; then
+    taskset_str="taskset -c $(seq -s, 0 $((core-1)))"
+    $taskset_str ./server $port $threadPoolSize
+else
+    echo "Invalid Core Count. It should be between 1 and $(nproc)."
+fi
+
 killChildren() {
     pkill -P $$  # Send the termination signal to all child processes
     exit 0  # Exit the script
 }
 
-# kill all the child processes using trap
 trap killChildren SIGINT SIGTERM
 
-# run the utilization script in the background
-bash utilization_script.sh $port &
-
-# run the server with port
-./server $port
+# ./utilization_script.sh $port &
