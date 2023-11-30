869688c14ff43b40d88f131562ec31a3bc3926b0
DebRC <roychowdhurydebdoot@gmail.com>
2023-11-30 12:33:31 +0530

    Added malloc error handling

--- v4/server/utils/circular_queue.c
+++ v4/server/utils/circular_queue.c
@@ -3,56 +3,62 @@
 #include "circular_queue.h"
 
 // Initialize the circular queue
-void initQueue(CircularQueue *q) {
+void initQueue(CircularQueue *q)
+{
     q->size = 0;
     q->front = NULL;
 }
 
 // Check if the queue is empty
-int isEmpty(CircularQueue *q) {
+int isEmpty(CircularQueue *q)
+{
     return q->size == 0;
 }
 
 // Enqueue an item
-int enqueue(CircularQueue *q, int item) {
-    Node * newNode = (Node *)malloc(sizeof(Node));
-    newNode->val=item;
-    newNode->next=q->front;
-    q->front=newNode;
+int enqueue(CircularQueue *q, int item)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL)
+        return -1;
+    newNode->val = item;
+    newNode->next = q->front;
+    q->front = newNode;
     q->size++;
     return 0;
 }
 
 // Dequeue an item
-int dequeue(CircularQueue *q) {
-    if (isEmpty(q)) {
-        printf("Queue is empty, cannot dequeue.\n");
+int dequeue(CircularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("QUEUE UNDERFLOW\n");
         return -1;
     }
-
-    Node* frontNode=NULL;
-    if(q->front){
-        frontNode=q->front;
-    }
+    Node *frontNode = q->front;
 
     int item = frontNode->val;
-    q->front=frontNode->next;
+    q->front = frontNode->next;
     free(frontNode);
     q->size--;
     return item;
 }
 
 // Count the number of items in the queue
-int getSize(CircularQueue *q) {
+int getSize(CircularQueue *q)
+{
     return q->size;
 }
 
 // Free the memory used by the queue
-void freeQueue(CircularQueue *q) {
-    Node* temp;
-    while(q->front){
+void freeQueue(CircularQueue *q)
+{
+    Node *temp;
+    while (q->front)
+    {
         temp = q->front;
-        q->front=temp->next;
+        q->front = temp->next;
         free(temp);
     }
 }
